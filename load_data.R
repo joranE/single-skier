8e83745dd70b13e02b90741e134e3d24eae9df8d
Joran Elias <joran.elias@umontana.edu>
2015-04-21 14:17:21 -0600

    Load smaller data

--- load_data.R
+++ load_data.R
@@ -1,4 +1,4 @@
-DATA <- read_csv("fis.csv",
+system.time(DATA <- read_csv("fis1.csv",
 								col_names = TRUE,
-								col_types = "iiccccdccccccciiciiddd")
+								col_types = "iiccccdccccccciiciiddd"))
 DATA$cat2[DATA$cat2 == ""] <- NA
