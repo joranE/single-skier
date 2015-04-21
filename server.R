db242246fee32b16f7063bdbe4952d6fd111744d
Joran Elias <joran.elias@umontana.edu>
2015-04-21 14:17:15 -0600

    ui changes

--- server.R
+++ server.R
@@ -9,10 +9,14 @@ shinyServer(function(input, output, session) {
   #Update name selections based on nation selection
   observe({
     selected_nation <- input$nationInput
-    updateSelectInput(session,inputId = "nameInput",choices = c("",sort(unique(DATA$name[DATA$nation == selected_nation]))))
+    updateSelectInput(session,
+                      inputId = "nameInput",
+                      choices = c("",sort(unique(DATA$name[DATA$nation == selected_nation]))))
   })
   
   #Create plots
+  
+  #Major international plots
   output$plotDst1 <- renderPlot({
     plot_dst(nm = input$nameInput,type = "points")
   })
@@ -21,12 +25,33 @@ shinyServer(function(input, output, session) {
     plot_dst(nm = input$nameInput,type = "rank")
   })
   
+  output$plotDst3 <- renderPlot({
+    plot_dst(nm = input$nameInput,type = "mpb")
+  })
+  
   output$plotSpr1 <- renderPlot({
-    plot_spr(nm = input$nameInput)
+    plot_spr(nm = input$nameInput,type = "rank")
   })
   
   output$plotSpr2 <- renderPlot({
     plot_spr_bar(nm = input$nameInput,byTech = TRUE)
   })
+  
+  #General FIS plots
+  output$plotDst4 <- renderPlot({
+    plot_dst(nm = input$nameInput,type = "points",maj_int = FALSE)
+  })
+  output$plotSpr3 <- renderPlot({
+    plot_spr(nm = input$nameInput,type = "points",maj_int = FALSE)
+  })
+  
+  output$plotSpr4 <- renderPlot({
+    plot_spr_bar(nm = input$nameInput,byTech = TRUE,maj_int = FALSE)
+  })
+  
+  gen_tables <- reactive({ath_summary(nm = input$nameInput)})
+  output$wjcTable <- renderTable({gen_tables()$ath_wjc})
+  output$u23Table <- renderTable({gen_tables()$ath_u23})
+  output$majTable <- renderTable({gen_tables()$ath_maj})
 }
 )
--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ source("load_nations.R")
 
 shinyUI(dashboardPage(
   #Header
-  dashboardHeader(title = "Basic dashboard"),
+  dashboardHeader(title = "Statistical Skier"),
   
   #Sidebar
   dashboardSidebar(
@@ -25,21 +25,116 @@ shinyUI(dashboardPage(
                 choices = "",
                 selected = NULL,
                 selectize = TRUE),
+    menuItem(
+      menuSubItem(text = "Summary",tabName= "athSummary"),
+      menuSubItem(text = "All FIS Results",tabName = "allFIS"),
+      menuSubItem(text = "Major International Results",tabName = "majInt"),
+      text = "Charts",
+      icon = icon("bar-chart")
+      ),
     hr(),
     helpText("Joran Elias - statisticalskier.com"),
     hr(),
-    helpText("Some generic help.")),
+    helpText("Choose a nation and then an individual from that nation. Click on one of the sidebar options 
+             to see the results."),
+    helpText("These plots show only one athlete at a time. Plots with small amounts of data may be omitted.")),
   
   #Body
   dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plotDst1", height = 350),title = "Distance"),
-      box(plotOutput("plotSpr1",height = 350),title = "Sprint")
-    ),
-    fluidRow(
-      box(plotOutput("plotDst2",height = 350),title = "Distance"),
-      box(plotOutput("plotSpr2",height = 350),title = "Sprint")
+    tabItems(
+      tabItem(
+        tabName = "athSummary",
+        fluidRow(
+          column(width = 6,
+                 box(tableOutput("wjcTable"),
+                     width = NULL,
+                     title = "World Junior Results",
+                     solidHeader = TRUE,
+                     collapsible = TRUE,
+                     status = "primary"),
+                 box(tableOutput("u23Table"),
+                     width = NULL,
+                     title = "U23 Results",
+                     solidHeader = TRUE,
+                     collapsible = TRUE,
+                     status = "primary")
+          ),
+          column(width = 6,
+                 box(tableOutput("majTable"),
+                     width = NULL,
+                     title = "Major Results",
+                     solidHeader = TRUE,
+                     collapsible = TRUE,
+                     status = "success")
+          )  
+        )
+      ),
+      tabItem(tabName = "allFIS",
+              fluidRow(
+                column(width = 6,
+                       box(plotOutput("plotDst4"),
+                           width = NULL,
+                           title = "Distance by FIS points",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "primary")
+                ),
+                
+                column(width = 6,
+                       box(plotOutput("plotSpr3"),
+                           width = NULL,
+                           title = "Sprint by FIS points",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "success"),
+                       box(plotOutput("plotSpr4"),
+                           width = NULL,
+                           title = "Sprint qualification rates",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "success")
+                )
+              )
+      ),
+      tabItem(tabName = "majInt",
+              fluidRow(
+                column(width = 6,
+                       box(plotOutput("plotDst3"),
+                           width = NULL,
+                           title = "Distance by Stand % Back from Median",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "primary"),
+                       box(plotOutput("plotDst1"),
+                           width = NULL,
+                           title = "Distance by FIS points",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "primary"),
+                       box(plotOutput("plotDst2"),
+                           width = NULL,
+                           title = "Distance by finishing place",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "primary")
+                ),
+                
+                column(width = 6,
+                       box(plotOutput("plotSpr1"),
+                           width = NULL,
+                           title = "Sprint by finishing place",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "success"),
+                       box(plotOutput("plotSpr2"),
+                           width = NULL,
+                           title = "Sprint qualification rates",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "success")
+                )
+            )
+        )
     )
   )
 ))
