7f3d7386614c7ee07b3664dc0796f03fcca32b04
Joran Elias <joran.elias@umontana.edu>
2015-04-22 09:17:20 -0600

    Remove unnecessary stuff

--- load_data.R
+++ load_data.R
@@ -1,4 +1,3 @@
-system.time(DATA <- read_csv("fis1.csv",
+DATA <- read_csv("fis1.csv",
 								col_names = TRUE,
-								col_types = "iiccccdccccccciiciiddd"))
-DATA$cat2[DATA$cat2 == ""] <- NA
+								col_types = "iiccccdccccccciiciiddd")
