ca7dd219f83989f91225103aa0851e3666d4b8e8
Joran Elias <joran.elias@umontana.edu>
2015-04-22 09:56:51 -0600

    Maybe commenting it out will keep shiny from thinking it needs to be able to run this

--- create_data.R
+++ create_data.R
@@ -1,24 +1,24 @@
-library(statskier)
+#library(statskier)
 
-con <- db_xc()
-x <- query(con,"select * from main")
-
-x$cat2[x$cat2 == ""] <- NA
-
-maj_ind <- x$cat1 %in% c('WC','WSC','OWG','TDS')
-x <- split(x,maj_ind)
-
-XC_FAC <- load_xc_conv()
-x[[2]] <- standardize_mpb_xc(mpb(con,x[[2]],0.5),XC_FAC)
-x[[1]]$mpb <- NA
-
-x <- do.call("rbind",x)
-x <- arrange(x,id)
-
-out <- subset(x,fisid %in% fisid[cat1 %in% c('WC','WSC','OWG','TDS')])
-
-write.table(x = out,
-            file = "fis1.csv",
-            sep = ",",
-            row.names = FALSE,
-            col.names = TRUE)
+# con <- db_xc()
+# x <- query(con,"select * from main")
+# 
+# x$cat2[x$cat2 == ""] <- NA
+# 
+# maj_ind <- x$cat1 %in% c('WC','WSC','OWG','TDS')
+# x <- split(x,maj_ind)
+# 
+# XC_FAC <- load_xc_conv()
+# x[[2]] <- standardize_mpb_xc(mpb(con,x[[2]],0.5),XC_FAC)
+# x[[1]]$mpb <- NA
+# 
+# x <- do.call("rbind",x)
+# x <- arrange(x,id)
+# 
+# out <- subset(x,fisid %in% fisid[cat1 %in% c('WC','WSC','OWG','TDS')])
+# 
+# write.table(x = out,
+#             file = "fis1.csv",
+#             sep = ",",
+#             row.names = FALSE,
+#             col.names = TRUE)
