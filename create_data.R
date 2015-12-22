78b02dc546022ec4e99e0193c9cfd7ba0492ce9a
Joran Elias <joran.elias@umontana.edu>
2015-12-22 16:13:43 -0700

    update create_data.R

--- create_data.R
+++ create_data.R
@@ -1,22 +1,24 @@
-library(statskier)
+library(statskier2)
 library(dplyr)
 
-con <- db_xc()
-x <- query(con,"select * from main")
+conl <- db_xc_local()
+x <- ss_query(conl,"select * from main")
 
 x$cat2[x$cat2 == ""] <- NA
 
 maj_ind <- x$cat1 %in% c('WC','WSC','OWG','TDS')
 x <- split(x,maj_ind)
 
-XC_FAC <- load_xc_conv()
-x[[2]] <- standardize_mpb_xc(mpb(con,x[[2]],0.5),XC_FAC)
+#XC_FAC <- load_xc_conv()
+x[[2]] <- x[[2]] %>%
+  mpb() %>%
+  standardize_mpb()
 x[[1]]$mpb <- NA
 
 x <- do.call("rbind",x)
 x <- dplyr::arrange(x,id)
 
-out <- subset(x,fisid %in% fisid[cat1 %in% c('WC','WSC','OWG','TDS')])
+out <- dplyr::filter(x,fisid %in% fisid[cat1 %in% c('WC','WSC','OWG','TDS')])
 
 file.remove("fis.sqlite3")
 fis_db <- src_sqlite("fis.sqlite3",create = TRUE)
